7c00c647f284ebf9ff4e8a0c935e744c59d36aae
meshackkirwachumba <meshackkirwachumba@gmail.com>
2023-12-14 10:09:46 +0300

    form done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,12 +7,27 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import NoProf from "../assets/NoProfile.jpg";
 import CustomButton from "../components/CustomButton";
-import { BsPersonFill } from "react-icons/bs";
+import { BsFiletypeGif, BsPersonFill } from "react-icons/bs";
+import { BiImages, BiSolidVideo } from "react-icons/bi";
+import TextInput from "../components/TextInput";
+import { useForm } from "react-hook-form";
+import Loading from "../components/Loading";
 
 function Home() {
   const { user } = useSelector((state) => state.user);
   const [friendRequest, setFriendRequest] = useState(requests);
   const [suggestedFriends, setSuggestedFriends] = useState(suggest);
+  const [errMsg, setErrMsg] = useState("");
+  const [file, setFile] = useState(null);
+  const [posting, setPosting] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({});
+
+  const handlePostSubmit = async (data) => {};
 
   return (
     <div
@@ -71,9 +86,111 @@ function Home() {
            flex-col
            gap-6
            overflow-y-auto
+           rounded-lg
          "
         >
-          MIDDLE
+          <form
+            onSubmit={handleSubmit(handlePostSubmit)}
+            className="bg-primary px-4 rounded-lg"
+          >
+            <div className="flex items-center gap-2 py-4 border-b border-[#66666645]">
+              <img
+                src={user?.profileUrl || NoProf}
+                alt={user?.firstName}
+                className="w-14 h-14 object-cover rounded-full"
+              />
+              <TextInput
+                inputStyles="w-full rounded-full py-5"
+                placeholder="What's on your mind?"
+                name="description"
+                register={register("description", {
+                  required: "Write something about post",
+                })}
+                error={errors.description ? errors.description?.message : ""}
+              />
+            </div>
+            {errMsg?.message && (
+              <span
+                role="alert"
+                className={`
+                text-sm
+                ${
+                  errMsg?.status === "failed"
+                    ? "text-[#f64949fe]"
+                    : "text-[#2ba150fe]"
+                } 
+               `}
+              >
+                {errMsg?.message}
+              </span>
+            )}
+
+            <div className="flex items-center justify-between py-4">
+              <label
+                htmlFor="imgUpload"
+                className="
+                flex items-center gap-1 text-base text-ascent-2 hover:text-ascent-1 cursor-pointer
+              "
+              >
+                <input
+                  type="file"
+                  onChange={(e) => setFile(e.target.files[0])}
+                  className="hidden"
+                  id="imgUpload"
+                  data-max-size="5120"
+                  accept=".jpg, .jpeg, .png"
+                />
+                <BiImages />
+                <span>Image</span>
+              </label>
+              <label
+                htmlFor="videoUpload"
+                className="
+                flex items-center gap-1 text-base text-ascent-2 hover:text-ascent-1 cursor-pointer
+              "
+              >
+                <input
+                  type="file"
+                  onChange={(e) => setFile(e.target.files[0])}
+                  className="hidden"
+                  id="videoUpload"
+                  data-max-size="5120"
+                  accept=".mp4, .wav"
+                />
+                <BiSolidVideo />
+                <span>Video</span>
+              </label>
+              <label
+                htmlFor="vgifUpload"
+                className="
+                flex items-center gap-1 text-base text-ascent-2 hover:text-ascent-1 cursor-pointer
+              "
+              >
+                <input
+                  type="file"
+                  onChange={(e) => setFile(e.target.files[0])}
+                  className="hidden"
+                  id="vgifUpload"
+                  data-max-size="5120"
+                  accept=".gif"
+                />
+                <BsFiletypeGif />
+                <span>Gif</span>
+              </label>
+
+              <div>
+                {posting ? (
+                  <Loading />
+                ) : (
+                  <CustomButton
+                    type="submit"
+                    title={"Post"}
+                    containerStyles="bg-[#0444a4] text-white py-1 px-6 rounded-full font-semibold text-sm"
+                  />
+                )}
+              </div>
+            </div>
+          </form>
         </div>
 
         {/* RIGHT CONTAINER */}
